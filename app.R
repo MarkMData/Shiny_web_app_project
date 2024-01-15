032f905cfa1eb319c1b3ba6638766989d835bada
MarkMData <150466955+MarkMData@users.noreply.github.com>
2024-01-15 16:51:51 +0000

    Upload files for shiny web app project

---
+++ app.R
@@ -0,0 +1,366 @@
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(calendR)
+library(shiny)
+library(leaflet)
+library(shinythemes)
+library(DT)
+library(sf)
+
+source("project_functions.R")
+load("project_RData_files/sites.RData")
+
+weather_options <- c(
+  "Air temperature (\u00B0C)", "Relative humidity (%)",
+  "Wind speed (kn)", "Visibility (m)")
+data_options <- list(
+  "Raw hourly data",
+  "Daily aggregates" = c("Daily average",
+                         "Daily maxima", "Daily minima"),
+  "Monthly aggregates" = c("Monthly average",
+                           "Monthly maxima", "Monthly minima")
+)
+months = c(1:11)
+names(months) <- month.name[months]
+raw_options <- c("Annual", "Month", "Week", "Day")
+daily_options <- c("Annual", "Month", "Week")
+monthly_options <- c("Annual", "Month")
+weeks_list <- as_date(seq(dmy_hm("1/1/2020 00:00"),
+                          dmy_hm("30/11/2021 23:00"),
+                          "week")
+                      )
+
+# UI ----------------------------------------------------------------------
+
+ui <- navbarPage(
+  "R-Project", theme = shinytheme("flatly"),
+
+# UI Tab 1 with meteorological elements -----------------------------------
+
+  tabPanel(
+    "Meteorological variables",
+    # Base map
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          h4(p(paste(
+            "Meteorological data collected from 20 sites across the",
+            "United Kingdom from Jan to Nov 2021")),
+            style = "line-height:1.75"
+          ),
+          selectizeInput(
+            "sitesM", label = "Weather stations",
+            choices = sites$Site_Name, multiple = TRUE,
+            options = list(placeholder = "Select up to five sites",
+                           maxItems = 5)
+          ),
+          # Select weather variable
+          selectizeInput(
+            "varM", label = "Weather variables", choices = weather_options,
+            options = list(placeholder = "Select weather variable")
+          ),
+          # Select data output type
+          selectizeInput(
+            "data_output", label = "Data output", choices = data_options,
+            options = list(placeholder = "Select data output")
+          ),
+          #  Conditional output selections when raw data is selected
+          conditionalPanel(
+            condition = "input.data_output == 'Raw hourly data'",
+            selectizeInput(
+              "raw", label = "Time/date options", choices = raw_options,
+              options = list(placeholder = "Select time/date output")
+            ),
+            # Raw data with single day selected
+            conditionalPanel(
+              condition = "input.raw == 'Day'",
+              dateInput(
+                "day_raw", label = "Select date", value = "2021-01-01",
+                min = "2021-01-01", max = "2021-11-30",
+                format = "d-MM-yyyy"
+              )
+            ),
+            # Raw data with week selected
+            conditionalPanel(
+              condition = "input.raw == 'Week'",
+              selectizeInput(
+                "week_raw", label = "Select week", choices = weeks_list,
+                options = list(placeholder = "Week beginning")
+              )
+            ),
+            # Raw data with month selected
+            conditionalPanel(
+              condition = "input.raw == 'Month'",
+              selectizeInput(
+                "month_raw", label = "Select month", choices = months,
+                options = list(placeholder = "Month")
+              )
+            )
+          ),
+          # Conditional output selections when daily aggregates are selected
+          conditionalPanel(
+            condition = "input.data_output == 'Daily average' ||
+                         input.data_output == 'Daily maxima' ||
+                         input.data_output == 'Daily minima'",
+            selectizeInput(
+              "daily", label = "Time/date options", choices = daily_options,
+              options = list(placeholder = "Select time/date output")
+            ),
+            # Daily aggregates with week selected
+            conditionalPanel(
+              condition = "input.daily == 'Week'",
+              selectizeInput(
+                "week_daily", label = "Select week", choices = weeks_list,
+                options = list(placeholder = "Week beginning")
+              )
+            ),
+            # # Daily aggregates with month selected
+            conditionalPanel(
+              condition = "input.daily == 'Month'",
+              selectizeInput(
+                "month_daily", label = "Select month", choices = months,
+                options = list(placeholder = "Month")
+              )
+            )
+          )
+        ),
+        mainPanel(
+          tabsetPanel(
+            tabPanel("Map",
+              leafletOutput("mapM", height = 550)
+            ),
+            tabPanel("Plot",
+                     br(),
+                     plotOutput("meteo", height = 500)
+            ),
+            tabPanel(
+              "Table",
+              h4(paste("Daily averages for selected sites from",
+              "24/11/2021 to 30/11/2021")
+              ),
+              br(),
+              div(dataTableOutput("table"), style = "font-size:60%")
+            ),
+            tabPanel(
+              "Downloads",
+              br(),
+              h4(paste("Ensure at least one site has been selected",
+              "before attempting download")),
+              br(),
+              h5("Download report with map, plot, and table for selected sites"),
+              downloadButton("report_download", label = "Download report"),
+              br(),
+              br(),
+              h5("Download table for selected sites as csv file"),
+              downloadButton("csv_download", label = "Download table")
+            )
+          )
+        )
+      )
+    )
+  ),
+
+# UI Tab 2 with Hutton criteria bits --------------------------------------
+
+  tabPanel(
+    "Hutton criteria",
+    # Base map
+    fixedPanel(
+      id = "mapH_panel",
+      top = 60, bottom = 0, right = 0, left = 0,
+      leafletOutput("mapH", height = "calc(100vh - 50px)")
+    ),
+    fixedPanel(
+      id = "H_info_panel",
+      top = 150, left = 100, right = "auto", bottom = "auto",
+      width = 350, height = "auto",
+      wellPanel(
+        h4(p(paste(
+          "The Hutton criteria is a diagnostic used to assess the risk of",
+          "blight forming on potato crops.")),
+          p(paste(
+            "For the Hutton criteria to be met, both of the following",
+            "conditions must occur on two consecutive days:")
+          ),
+          tags$ol(
+            tags$li("Each day has a minimum temperature of 10°C"),
+             tags$li("Each day has at least six hours with a relative humidity ≥ 90%")
+          ),
+          style = "line-height:1.75"
+        )
+      )
+    ),
+    # Panel with input controls
+    fixedPanel(
+      id = "H_options_panel",
+      top = 80, left = "auto", right = 70, bottom = "auto",
+      width = 450, height = "auto",
+      wellPanel(
+        selectizeInput(
+          "siteH", "Select site", choices = sites$Site_Name,
+          options = list(placeholder = "Select data output")),
+        selectInput(
+          "mthH", "Select month", choices = months),
+        br(),
+        plotOutput("calendar")
+      )
+    )
+  )
+)
+
+
+# Server ------------------------------------------------------------------
+
+server <- function(input, output, session){
+
+#  Outputs for meteorological tab -----------------------------------------
+
+   # plotting base map
+  output$mapM <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.PositronNoLabels,
+                       options = providerTileOptions(opacity = 1)) %>%
+      setView(lng = -6, lat = 54.7, zoom = 5) %>%
+      setMaxBounds(-30, 45, 40, 64)
+  })
+  
+  # Markers
+  observe({
+    leafletProxy("mapM") %>%
+      clearMarkers()
+    if (!is.null(input$sitesM)){
+      markers <- sites %>%
+        filter(Site_Name %in% input$sitesM)
+      leafletProxy("mapM") %>%
+      clearMarkers() %>%
+      addMarkers(lat = markers$Latitude,
+                 lng = markers$Longitude,
+                 label = markers$Site_Name)
+    }
+  })
+  
+  # Meteorological plot
+  month_sel <- reactiveVal(NULL)
+  week_sel <- reactiveVal(NULL)
+  day_sel <- reactiveVal(NULL)
+  sites_sel <- reactiveVal()
+  var_sel <- reactiveVal()
+  data_sel <- reactiveVal()
+  
+  observe({
+    # Setting month value
+    if (input$data_output == "Raw hourly data" & input$raw == "Month") {
+      month_sel(input$month_raw)
+    } else if (
+      (input$data_output == 'Daily average' |
+       input$data_output == 'Daily maxima' |
+       input$data_output == 'Daily minima') &
+      input$daily == "Month") {
+      month_sel(input$month_daily)
+    } else {
+      month_sel(NULL)
+    }
+    # Setting week value
+    if (input$data_output == "Raw hourly data" & input$raw == "Week") {
+      week_sel(week(as_date(input$week_raw)))
+    } else if (
+      (input$data_output == 'Daily average' |
+       input$data_output == 'Daily maxima' |
+       input$data_output == 'Daily minima') &
+      input$daily == "Week") {
+      week_sel(week(as_date(input$week_daily)))
+    } else {
+      week_sel(NULL)
+    }
+    # Setting day value
+    if (input$data_output == "Raw hourly data" & input$raw == "Day") {
+      day_sel(input$day_raw)
+    } else {
+      day_sel(NULL)
+    }
+    sites_sel(input$sitesM)
+    var_sel(input$varM)
+    data_sel(input$data_output)
+  })
+  # Plotting chart with selected parameters using plot function
+  output$meteo <- renderPlot(
+    if (!is.null(sites_sel())){
+      plt_function(selected_sites = sites_sel(),
+                   selected_var = var_sel(),
+                   data_type = data_sel(),
+                   Mth = month_sel(),
+                   Wk = week_sel(),
+                   Day = day_sel()
+      )
+  })
+  
+  
+
+
+# Table of selected sites -----------------------------------------------
+  table <- reactive({table_function(input$sitesM)})
+  output$table <- renderDataTable(
+    if (!is.null(input$sitesM)) {
+      table()
+    }
+    )
+ 
+
+# # Downloads -------------------------------------------------------------
+  # Table csv download
+  output$csv_download <- downloadHandler(
+    filename = function() {
+      "table.csv"
+    },
+    content = function(file) {
+      write.csv(table(), file)
+    }
+  )
+  output$report_download <- downloadHandler(
+    filename = "report.docx",
+    content = function(file) {
+      params <- list(month_sel = month_sel(),
+                     week_sel = week_sel(),
+                     day_sel = day_sel(),
+                     sites_sel = sites_sel(),
+                     var_sel = var_sel(),
+                     data_sel = data_sel()
+                     )
+      rmarkdown::render("report.Rmd",
+             output_file = file,
+             output_format = "word_document",
+             params = params,
+             envir = new.env(parent = globalenv())
+      )
+    }
+  )
+  
+# Outputs for Hutton tab --------------------------------------------------
+
+  selected_siteH <- reactive({
+    get(load(paste0("project_RData_files/site_",
+                    sites$Site_ID[sites$Site_Name == input$siteH],
+                    ".Rdata"))
+    )
+  })
+  selected_mthH <- reactive({input$mthH})
+  output$calendar <- renderPlot({
+    f_hutton(selected_siteH(), as.numeric(selected_mthH()))
+  })
+  output$mapH <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.PositronNoLabels,
+                       options = providerTileOptions(opacity = 1)) %>%
+      setView(lng = sites$Longitude[sites$Site_Name == input$siteH],
+              lat = sites$Latitude[sites$Site_Name == input$siteH],
+              zoom = 6) %>%
+      setMaxBounds(-30, 45, 40, 64) %>%
+      addMarkers(lng = sites$Longitude[sites$Site_Name == input$siteH],
+                 lat = sites$Latitude[sites$Site_Name == input$siteH],
+                 label = sites$Site_Name[sites$Site_Name == input$siteH]
+                 )
+  })
+}
+
+shinyApp(ui, server)
---
+++ project_functions.R
@@ -0,0 +1,291 @@
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(calendR)
+library(ggthemes)
+library(mapview)
+library(leaflet)
+library(webshot)
+library(png)
+load("project_RData_files/sites.RData")
+
+# Function to plot meteorological data ------------------------------------
+
+plt_function <- function(selected_sites,
+                         selected_var,
+                         data_type = "Raw hourly data",
+                         Mth = NULL,
+                         Wk = NULL,
+                         Day = NULL){
+  # Match selected weather variable with with DF column name
+  weather_vars <- c("Air temperature (\u00B0C)" = "air_temperature",
+                    "Relative humidity (%)" = "rltv_hum",
+                    "Wind speed (kn)" = "wind_speed",
+                    "Visibility (m)" = "visibility")
+  variable <- weather_vars[selected_var]
+  # Create a of data frames for selected sites
+  df_list <- list()
+  for (x in seq(1,length(selected_sites))){
+    df_list[[x]] <- get(
+      load(paste0("project_RData_files/site_",
+                  sites$Site_ID[sites$Site_Name == selected_sites[x]],
+                  ".Rdata")
+      )
+    )
+  }
+  # Loop through the list of data frames cleaning things up
+  for (y in seq(1,length(selected_sites))){
+    df_list[[y]] <- df_list[[y]] %>%
+      # Filter out 29th Feb
+      filter(!(month == 2 & day == 29)) %>%
+      # Fix year, create a date/time column
+      mutate(year = 2021,
+             date_time = make_datetime(year, month, day, hour),
+             week = week(date_time)
+      )
+    # Create a data frame with complete list of days and hours
+    complete_hours <- tibble(
+      date_time = seq(dmy_hm("1/1/2021 00:00"),
+                      dmy_hm("30/11/2021 23:00"), "hour")
+    )
+    # Fill in missing dates/hours in data sets
+    df_list[[y]] <- full_join(complete_hours, df_list[[y]], by = "date_time")
+    # Remove duplicate dates
+    df_list[[y]] <- df_list[[y]][!duplicated(df_list[[y]]$date_time), ]
+    # Filter by date/time output if required
+    if (!is_null(Mth)) {
+      df_list[[y]] <- df_list[[y]] %>% filter(month == Mth)
+    }
+    if (!is_null(Wk)) {
+      df_list[[y]] <- df_list[[y]] %>% filter(week == Wk)
+    }
+    if (!is_null(Day)) {
+      df_list[[y]] <- df_list[[y]] %>% filter(
+        day == day(Day) & month == month(Day)
+      )
+    }
+    # If raw data output selected
+    if (data_type == "Raw hourly data") {
+      # Select only date/time and variable to be plotted
+      df_list[[y]] <- df_list[[y]] %>% select(date_time, variable)
+      # Re-name variable column with sites name
+      colnames(df_list[[y]]) <-  c("date_time", selected_sites[y])
+    }
+    
+    # If aggregations selected, perform  aggregation and
+    # then select only date/time  and variable of interest.
+    
+    # If daily average output selected
+    if (data_type == "Daily average") {
+      df_list[[y]] <- df_list[[y]] %>% group_by(year, month, day) %>%
+        summarise(variable = mean(!!sym(variable), na.rm = TRUE)
+        ) %>%
+        mutate(date_time = make_date(year = year, month = month, day = day)) %>%
+        ungroup() %>%
+        select(date_time, variable)
+    }
+    # If daily max output selected
+    if (data_type == "Daily maxima") {
+      df_list[[y]] <- df_list[[y]] %>% group_by(year, month, day) %>%
+        summarise(variable = max(!!sym(variable), na.rm = TRUE)
+        ) %>%
+        mutate(date_time = make_date(year = year, month = month, day = day)) %>%
+        ungroup() %>%
+        select(date_time, variable)
+    }
+    # If daily min output selected
+    if (data_type == "Daily minima") {
+      df_list[[y]] <- df_list[[y]] %>% group_by(year, month, day) %>%
+        summarise(variable = min(!!sym(variable), na.rm = TRUE)
+        ) %>%
+        mutate(date_time = make_date(year = year, month = month, day = day)) %>%
+        ungroup() %>%
+        select(date_time, variable)
+    }
+    # If monthly average output selected
+    if (data_type == "Monthly average") {
+      df_list[[y]] <- df_list[[y]] %>% group_by(month) %>%
+        summarise(variable = mean(!!sym(variable), na.rm = TRUE)
+        )
+    }
+    # If monthly max output selected
+    if (data_type == "Monthly maxima") {
+      df_list[[y]] <- df_list[[y]] %>% group_by(month) %>%
+        summarise(variable = max(!!sym(variable), na.rm = TRUE)
+        )
+    }
+    # If monthly min output selected
+    if (data_type == "Monthly minima") {
+      df_list[[y]] <- df_list[[y]] %>% group_by(month) %>%
+        summarise(variable = min(!!sym(variable), na.rm = TRUE)
+        )
+    }
+    # Rename variable column with site name.
+    colnames(df_list[[y]]) <-  c("date_time", selected_sites[y])
+  }
+  # bring all data sets into one data frame
+  combined_dfs <- reduce(df_list, full_join, by = "date_time")
+  # Filter out a random NA row that was occurring in monthly aggregations
+  if (nrow(combined_dfs) == 12) {
+    combined_dfs <- combined_dfs %>%
+      filter(!is.na(date_time))
+  } 
+  # Change data to long format for plotting 
+  plt_data <- combined_dfs %>%
+    pivot_longer(cols = 1:length(selected_sites)+1,
+                 names_to = "Selected sites",
+                 values_to = selected_var
+    )
+  # Create plot with selected sites and variable of choice
+  y_var <- sym(selected_var)
+  if (data_type == "Raw hourly data"){
+    plt <- ggplot(data = plt_data, aes(date_time, !! y_var)) +
+      geom_line(aes(colour = `Selected sites`)) +
+      xlab(NULL) +
+      labs(title = paste0(selected_var," (",data_type,")")) +
+      theme(aspect.ratio = 1/2)
+  }
+  if (data_type != "Raw hourly data"){
+    plt <- ggplot(
+      data = plt_data, 
+      if (nrow(combined_dfs) == 11) {
+        aes(x = factor(month.name[date_time], levels = month.name),
+            y = !! y_var)
+      } else { 
+        aes(x = date_time, y = !! y_var)
+      }
+    ) +
+      geom_point(aes(colour = `Selected sites`)) +
+      xlab(NULL) +
+      labs(title = paste0(selected_var," (",data_type,")")) +
+      theme(aspect.ratio = 1/2)
+  }  
+  plt
+}
+
+
+# Table function ----------------------------------------------------------
+
+table_function <- function(selected_sites){
+  # Create a of data frames for selected sites
+  df_list <- list()
+  for (x in seq(1,length(selected_sites))){
+    df_list[[x]] <- get(
+      load(paste0("project_RData_files/site_",
+                  sites$Site_ID[sites$Site_Name == selected_sites[x]],
+                  ".Rdata")
+      )
+    )
+  }
+  # loop through list of DFs organising data
+  for (y in seq(1,length(selected_sites))){
+    df_list[[y]] <- df_list[[y]] %>%
+      # Fix year and add date_time column
+      mutate(year = 2021, date_time = make_datetime(year, month, day, hour))
+    # Create a data frame with complete days and hours for last week in Nov
+    complete_hours <- tibble(
+      date_time = seq(dmy_hm("24/11/2021 00:00"),
+                      dmy_hm("30/11/2021 23:00"), "hour")
+    )
+    # Fill in missing dates/hours in data sets
+    df_list[[y]] <- left_join(complete_hours, df_list[[y]], by = "date_time")
+    # Remove duplicate dates
+    df_list[[y]] <- df_list[[y]][!duplicated(df_list[[y]]$date_time), ] %>%
+      # Create daily averages for weather variables
+      group_by(year, month, day) %>%
+      summarise(Air_temperature = round(mean(air_temperature), digits = 2),
+                Relative_humidity = round(mean(rltv_hum), digits = 2),
+                Wind_speed = round(mean(wind_speed), digits = 2),
+                Visibility = round(mean(visibility), digits = 2)
+      ) %>%
+      # Add date and site name columns
+      mutate(Date = make_date(year = year, month = month, day = day),
+             Site_name = selected_sites[y]) %>%
+      ungroup() %>%
+      # select desired columns
+      select(Site_name,
+             Date,
+             Air_temperature,
+             Relative_humidity,
+             Wind_speed,
+             Visibility
+      )
+  }
+  # bring all data sets into one data frame
+  table <- reduce(df_list, full_join,by = c("Site_name",
+                                            "Date",
+                                            "Air_temperature",
+                                            "Relative_humidity",
+                                            "Wind_speed",
+                                            "Visibility"
+  )
+  )
+  table
+}
+
+# Hutton function to plot calendar ----------------------------------------
+
+f_hutton <- function(site, mth){
+  # Remove 29th of Feb
+  site_h <- site %>%
+    filter(!(month == 2 & day == 29)) %>% 
+    # Group by month and day and add cols with daily summaries for 
+    # number of hours above 90% humidity, and min temp at least 10 degrees
+    group_by(month, day) %>%
+    summarise(temp_10 = min(air_temperature) >= 10,
+              humid_90 = sum(rltv_hum >= 90)
+    ) %>%
+    # Add in cols for date (existing date col was giving me problems) and year
+    mutate(Year = 2021,
+           Date = make_date(Year, month, day)
+    ) %>%
+    ungroup()
+  # Create a DF with complete dates for 1/1/21 to 30/11/21
+  complete_dates <- tibble(Date = c(seq(as_date("2021/1/1"),
+                                        as_date("2021/11/30"), 1))
+                           )
+  # Join dates DF and site DF to create rows for missing dates
+  site_h <- full_join(complete_dates, site_h)
+  # Add row indicating if Hutton criteria met
+  site_h <- site_h %>%
+    mutate(H_crit_met = (lag(temp_10)) == TRUE &
+                   lag(temp_10,n=2) == TRUE &
+                   lag(humid_90) >= 6 &
+                   lag(humid_90, n = 2) >= 6,
+                   )
+  # Replace NA values in Hutton col with false, and select month
+  site_h$H_crit_met <- replace_na(site_h$H_crit_met, FALSE)         
+  hutton_mth <- site_h %>%
+    filter(month == mth)
+  # plot calendar of selected month, Hutton true = pink
+  calendR(year = 2021,
+          start = "M",
+          month = mth,
+          special.days = hutton_mth$H_crit_met,
+          special.col = c("white", "pink"),
+          weeknames = c("Mon", "Tue",  "Wed", "Thu", "Fri", "Sat", "Sun"),
+          subtitle = "Days when Hutton criteria met indicated in red",
+          subtitle.size = 12,
+          )
+}
+
+
+# Function to create map image for report ---------------------------------
+
+map_image_func <- function(sites_sel, time){
+  markers <- sites %>%
+    filter(Site_Name %in% sites_sel)
+  map <- leaflet() %>%
+            addProviderTiles(providers$CartoDB.PositronNoLabels,
+                          options = providerTileOptions(opacity = 0.7)) %>%
+            setView(lng = -3.43, lat = 55.37, zoom = 5) %>%
+            addMarkers(lat = markers$Latitude,
+                       lng = markers$Longitude,
+                       label = markers$Site_Name,
+                       labelOptions = labelOptions(noHide = T,
+                                                   direction = "bottom"
+                                                   )
+            )
+  mapshot(map, file = paste0(getwd(),"/map.png"))
+}
+
